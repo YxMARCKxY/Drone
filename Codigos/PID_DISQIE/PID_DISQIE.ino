e39571ad257fba3e6b7842737c4b4fbb57a4fe44
MARCELO RAMIREZ M <mramirez73@ucol.mx>
2021-11-24 08:32:05 -0600

    Version final funcional

--- Codigos/PID_DISQIE/PID_DISQIE.ino
+++ Codigos/PID_DISQIE/PID_DISQIE.ino
@@ -1,59 +1,96 @@
+//librerias
 #include <PID_v1.h>
 #include <Servo.h>
 #include <I2Cdev.h>
 #include <MPU6050.h>
 #include "Wire.h"
 
+//instancias
 MPU6050 sensor;
 Servo ESC;
 
+//variables del sensor
 int ax, ay, az;
 
-// Definir variables a las que nos conectaremos
+//declaracion de variables del pid
 double punto_ajuste, entrada, salida;
 
-// Especifique los enlaces y los parámetros de ajuste iniciales
-PID myPID(&entrada, &salida, &punto_ajuste,2,5,1, DIRECT);
+//INSTANCIA PID
+PID myPID(&entrada, &salida, &punto_ajuste,2,0,0.2, DIRECT);
+//PID myPID(&entrada, &salida, &punto_ajuste,0.679,5,1.897, DIRECT);
+//PID myPID(&entrada, &salida, &punto_ajuste,0.016,0.00005,0.000623, DIRECT); //el mejor hasta el momento
+//0.6
 
-
-void setup()
-{
+void setup(){
   
-// inicializamos las variables a las que estamos vinculados
-  entrada = angulo_actual();
-  punto_ajuste = 0;
+    //obtencion del angulo actual
+    entrada = angulo_actual();//-6000 a 6000
+    punto_ajuste =-1431;//angulo deseado
+    
+    //encendido del pid
+    myPID.SetOutputLimits(65,80);
+    myPID.SetMode(AUTOMATIC);
+    
+    Serial.begin(9600);     //Iniciando puerto serial
+    Wire.begin();           //Iniciando I2C  
+    sensor.initialize();    //Iniciando el sensor
+    
+    pinMode(3,OUTPUT);
+    ESC.attach(3);//pin del arduino que controlara el esc
 
-// enciende el PID
-  myPID.SetMode(AUTOMATIC);
-
-  Serial.begin(9600);     //Iniciando puerto serial
-  Wire.begin();           //Iniciando I2C  
-  sensor.initialize();    //Iniciando el sensor
-
-   
-  ESC.attach(8);//pin del arduino que controlara el esc
-  //pulsos para activar el 'ESC'
-  ESC.writeMicroseconds(1000);
-  //tiempo de espera para la activacion del esc
-  delay(5000);
+    
+    //pulsos para activar el 'ESC'
+    ESC.writeMicroseconds(1000);
+    //tiempo de espera para la activacion del esc
+    delay(5000);
 }
 
 int angulo_actual(){
     sensor.getAcceleration(&ax, &ay, &az);//obtiene variables del sensor MPU6050
-    return map(ay, -6000, 6000, -45, 45);
-  }
+    return ay;
+}
 
 
-void loop()
-{
-  entrada = angulo_actual();
-  myPID.Compute();
-  int  velocidad = map(salida, 0, 255, 0, 80);
+void loop(){
 
-  if(velocidad >= 0 && velocidad <=130){
-      ESC.write(velocidad);
-      Serial.println(velocidad);
-  }
+  long muestras = 0;
+  long cont = 1;
+  bool act = false;
   
-    
+  do{
+    int angulo_muestra = angulo_actual();
+    muestras+=angulo_muestra;
+    cont++;
+    if(cont==50)
+      act = true;
+  }while(act==false);
+
+  
+  entrada = muestras/50;//filtro de ruido
+  //entrada = angulo_actual();
+  myPID.Compute();
+
+  double velocidad=salida;
+        
+  if(velocidad >= 0 && velocidad <=130){
+      //ESC.write(velocidad);
+      
+ 
+      //Serial.print(velocidad);
+      //Serial.print("   --   ");
+      
+      //Serial.println(" grados");
+      //Serial.println();
+  }
+  Serial.print(velocidad);
+  Serial.print(",");
+  Serial.print(punto_ajuste);
+  Serial.print(",");
+  Serial.print(entrada);
+  Serial.print(",");
+  Serial.print(angulo_actual());
+  Serial.println();
+  
+  /*Serial.println(velocidad);
+  Serial.println(entrada);*/  
 }
